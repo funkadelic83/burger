b6a553ef5fbcdcf24214991d2b23e52c5887a8c9
funkadelic83 <mikedamanskis@gmail.com>
2019-04-23 08:54:52 -0700

    revise helper functions

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,16 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'mike',
-        password: '$aTan6969videoL0rd$',
-        database: 'burgers_db'
-    });
-};
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         user: 'pxypakhp1inrk1vr',
+//         password: 'mo7mow02jznhdekp',
+//         database: 'axj8wfcojxpvfpc3'
+//     });
+// };
 
 connection.connect(function(err) {
     if (err) {
